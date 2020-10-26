33f85e40879788a1b7a9145877a18c2e971cb710
faiza203 <fiazfaiza981@gmail.com>
2020-10-26 17:16:49 +0500

    Replace for to htmlFor

--- src/Components/AddNewTransaction.js
+++ src/Components/AddNewTransaction.js
@@ -3,11 +3,11 @@ export const AddNewTransaction = () => {
         <h3>Add new transaction</h3>
         <form id="form">
             <div className="form-control">
-                <label for="text">Text</label>
+                <label htmlFor="text">Text</label>
                 <input type="text" id="text" placeholder="Enter text..." />
             </div>
             <div className="form-control">
-                <label for="amount"
+                <label htmlFor="amount"
                 >Amount <br />
           (negative - expense, positive - income)</label
                 >
