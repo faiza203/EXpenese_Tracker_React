c7dd29d7c67d652ff8d737dd30082c784cce0dd5
faiza203 <fiazfaiza981@gmail.com>
2020-10-26 16:43:49 +0500

    Create TransactionList Element

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,8 +1,10 @@
 import {IncomeExpenses} from "./IncomeExpenses";
+import {TransactionList} from "./TransactionList"
 export const Container = () => {
     return <div id="container">  <h4>Your Balance</h4>
         <h1 id="balance">$0.00</h1>
         <IncomeExpenses />
+        <TransactionList />
     </div>
   }
   
