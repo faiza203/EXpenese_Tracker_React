9edd5157024a7947593a8cc9bad4921d3db7e617
faiza203 <fiazfaiza981@gmail.com>
2020-10-26 16:36:32 +0500

    Create IncomeExpenses Element

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,6 +1,8 @@
+import {IncomeExpenses} from "./IncomeExpenses";
 export const Container = () => {
-    return <div>  <h4>Your Balance</h4>
+    return <div id="container">  <h4>Your Balance</h4>
         <h1 id="balance">$0.00</h1>
+        <IncomeExpenses />
     </div>
   }
   
---
+++ src/Components/IncomeExpenses.js
@@ -0,0 +1,9 @@
+import React from "react";
+export const IncomeExpenses = () => {
+    return <div class="inc-exp-container">
+        <div>
+            <h4>Income</h4>
+            <p id="money-plus" class="money plus">+$0.00</p>
+        </div>
+    </div>
+}
