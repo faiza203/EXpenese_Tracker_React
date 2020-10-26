ec4689694e2b565e0ca90878dcbe5b46e7882f35
faiza203 <fiazfaiza981@gmail.com>
2020-10-26 16:29:48 +0500

    Create Container element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Header } from "./Components/Header";
+import { Container } from "./Components/Container"
 function App() {
   return (
     <div>
       <Header />
+      <Container />
     </div>
   );
 }
