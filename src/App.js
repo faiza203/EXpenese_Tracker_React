8d8840107e613076791c42e1ba0dc544c4b4851f
faiza203 <fiazfaiza981@gmail.com>
2020-10-26 16:23:28 +0500

    Create Header element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
-
+import {Header} from "./Components/Header"
 function App() {
   return (
     <div>
-    Hello World
+    <Header />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,3 @@
+export const Header = () => {
+    return <h2>Expense Tracker</h2>
+}
