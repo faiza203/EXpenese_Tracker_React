1210fdd22e0f3f6193782cb60a582a15f8527c4d
faiza203 <fiazfaiza981@gmail.com>
2020-10-26 17:22:21 +0500

    Add useState

--- src/Components/AddNewTransaction.js
+++ src/Components/AddNewTransaction.js
@@ -1,10 +1,13 @@
+import useState from "react";
 export const AddNewTransaction = () => {
+    const { text, setText } = useState("");
+    const { amount, setAmount } = useState(0);
     return <div>
         <h3>Add new transaction</h3>
         <form id="form">
             <div className="form-control">
                 <label htmlFor="text">Text</label>
-                <input type="text" id="text" placeholder="Enter text..." />
+                <input type="text" id="text" value={text} placeholder="Enter text..." onChange={(e) => e.target.value(setText)}/>
             </div>
             <div className="form-control">
                 <label htmlFor="amount"
