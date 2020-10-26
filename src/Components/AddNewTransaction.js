e0718e6e53a19da06a5535651eba1c6b3b98041d
faiza203 <fiazfaiza981@gmail.com>
2020-10-26 16:54:29 +0500

    Create AddNewTransaction Element

---
+++ src/Components/AddNewTransaction.js
@@ -0,0 +1,19 @@
+export const AddNewTransaction = () => {
+    return <div>
+        <h3>Add new transaction</h3>
+        <form id="form">
+            <div class="form-control">
+                <label for="text">Text</label>
+                <input type="text" id="text" placeholder="Enter text..." />
+            </div>
+            <div class="form-control">
+                <label for="amount"
+                >Amount <br />
+          (negative - expense, positive - income)</label
+                >
+                <input type="number" id="amount" placeholder="Enter amount..." />
+            </div>
+            <button class="btn">Add transaction</button>
+        </form>
+    </div>
+}
--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,10 +1,12 @@
 import {IncomeExpenses} from "./IncomeExpenses";
 import {TransactionList} from "./TransactionList"
+import {AddNewTransaction} from "./AddNewTransaction"
 export const Container = () => {
     return <div id="container">  <h4>Your Balance</h4>
         <h1 id="balance">$0.00</h1>
         <IncomeExpenses />
         <TransactionList />
+        <AddNewTransaction />
     </div>
   }
   
