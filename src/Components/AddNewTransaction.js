f679f72b463402ceeeec4675eec4142d2713af01
faiza203 <fiazfaiza981@gmail.com>
2020-10-27 13:44:59 +0500

    Resolve an error that was occured in useState

--- src/Components/AddNewTransaction.js
+++ src/Components/AddNewTransaction.js
@@ -1,20 +1,20 @@
-import useState from "react";
+import{ useState} from "react";
 export const AddNewTransaction = () => {
-    const { text, setText } = useState("");
-    const { amount, setAmount } = useState(0);
+    const [text, setText] = useState("");
+    const [amount, setAmount] = useState(0);
     return <div>
         <h3>Add new transaction</h3>
         <form id="form">
             <div className="form-control">
                 <label htmlFor="text">Text</label>
-                <input type="text" id="text" value={text} placeholder="Enter text..." onChange={(e) => e.target.value(setText)}/>
+                <input type="text" id="text" value={text} placeholder="Enter text..." onChange={(e) => setText(e.target.value)} />
             </div>
             <div className="form-control">
                 <label htmlFor="amount"
                 >Amount <br />
           (negative - expense, positive - income)</label
                 >
-                <input type="number" id="amount" placeholder="Enter amount..." />
+                <input type="number" value={amount}  onChange={(e) => setAmount(e.target.value)} id="amount" placeholder="Enter amount..." />
             </div>
             <button className="btn">Add transaction</button>
         </form>
