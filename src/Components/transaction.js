4f17b6c0754e4f461a37e31e75d023fc12cf7368
faiza203 <fiazfaiza981@gmail.com>
2020-10-27 14:36:12 +0500

    Add function to detect amount is minus or plus

--- src/Components/transaction.js
+++ src/Components/transaction.js
@@ -1,5 +1,7 @@
 export const Transaction = ({ transaction }) => {
-    return <li className="minus">
-        {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
+    const sign = transaction.amount > 0 ? "-" : "+";
+    const transactionClassName = transaction.amount > 0 ? "minus" : "plus";
+    return <li className={transactionClassName}>
+        {transaction.text} <span>{sign} $ {Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
     </li>
 }
