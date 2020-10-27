a25ef27fe5095792921d4226637da244cd22ba7d
faiza203 <fiazfaiza981@gmail.com>
2020-10-27 15:20:03 +0500

    Create expense and income

--- src/Components/IncomeExpenses.js
+++ src/Components/IncomeExpenses.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "./../Context/globalState";
+
 export const IncomeExpenses = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map((transaction) => transaction.amount);
+    const income = (amounts.filter((item) => parseInt(item) > 0).reduce((acc, item) => (acc += parseInt(item)), 0) * 1)
+        .toFixed(2);
+    const expense = (amounts.filter((item) => parseInt(item) < 0).reduce((acc, item) => (acc += parseInt(item)), 0) * -1)
+        .toFixed(2);
     return <div className="inc-exp-container">
         <div>
             <h4>Income</h4>
-            <p id="money-plus" className="money plus">+$0.00</p>
+            <p id="money-plus" className="money plus">+${income}</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p id="money-minus" className="money minus">-$0.00</p>
+            <p id="money-minus" className="money minus">-${expense}</p>
         </div>
 
     </div>
--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,6 +1,6 @@
 export const Transaction = ({ transaction }) => {
-    const sign = transaction.amount > 0 ? "-" : "+";
-    const transactionClassName = transaction.amount > 0 ? "minus" : "plus";
+    const sign = transaction.amount > 0 ? "+" : "-";
+    const transactionClassName = transaction.amount > 0 ? "plus" : "minus";
     return <li className={transactionClassName}>
         {transaction.text} <span>{sign} $ {Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
     </li>
