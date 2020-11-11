8b75245da8e7820a75c8fc15d474bfbb92079231
faiza203 <fiazfaiza981@gmail.com>
2020-11-11 16:40:40 +0500

    Initialize firebaseConfig

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,13 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+importScripts("https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js")
+importScripts("https://www.gstatic.com/firebasejs/8.0.1/firebase-analytics.js")
 const firebaseConfig = {
-    apiKey: "AIzaSyAZX5VgMVNTyz-J_UUoCFuXIFK48pk7mzU",
-    authDomain: "common-50c43.firebaseapp.com",
-    databaseURL: "https://common-50c43.firebaseio.com",
-    projectId: "common-50c43",
-    storageBucket: "common-50c43.appspot.com",
-    messagingSenderId: "670911494230",
-    appId: "1:670911494230:web:f3b275d099e5763c75a3f7",
-    measurementId: "G-VWTHY9S6DH"
-  };
+  apiKey: "AIzaSyAZX5VgMVNTyz-J_UUoCFuXIFK48pk7mzU",
+  authDomain: "common-50c43.firebaseapp.com",
+  databaseURL: "https://common-50c43.firebaseio.com",
+  projectId: "common-50c43",
+  storageBucket: "common-50c43.appspot.com",
+  messagingSenderId: "670911494230",
+  appId: "1:670911494230:web:f3b275d099e5763c75a3f7",
+  measurementId: "G-VWTHY9S6DH"
+};
+firebase.initializeApp(firebaseConfig);
