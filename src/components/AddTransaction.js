20c49c9e9443b6d30e464acfdabb730818df41b7
faiza203 <fiazfaiza981@gmail.com>
2020-11-12 12:42:51 +0500

    Update Transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,6 +1,7 @@
 import React, {useState, useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
-import {initNotification} from './../service/firebase-service'
+import {initNotification} from './../service/firebase-service';
+
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
