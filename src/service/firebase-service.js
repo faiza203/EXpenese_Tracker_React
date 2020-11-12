42be0d2ed0c52c21f62cebc7080f652f1eeb35af
faiza203 <fiazfaiza981@gmail.com>
2020-11-12 11:18:22 +0500

    Set function on granted state

--- src/service/firebase-service.js
+++ src/service/firebase-service.js
@@ -1,3 +1,15 @@
-export function configureNotification(){
-Notification.requestPermission().then((permission) => {console.log(permission)}
-)}
+export function configureNotification() {
+    Notification.requestPermission().then((permission) => {
+       if(permission === "granted"){
+        messaging.getToken({ vapidKey: '<YOUR_PUBLIC_VAPID_KEY_HERE>' }).then((currentToken) => {
+            if (currentToken) {
+                 console.log("TOKEN =>" , currentToken);
+            } else {
+                console.log('No registration token available. Request permission to generate one.');
+            }
+        }).catch((err) => {
+            console.log('An error occurred while retrieving token. ', err);
+        })
+       }
+    })
+}
